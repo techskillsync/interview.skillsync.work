e7e299b4d4d492a8d0515b233520770fed6911d8
Rahul Goyal <imrahulg01@gmail.com>
2024-10-03 00:03:42 +0530

    update: added testing route for speech analysis

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -1,9 +1,15 @@
 import os
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, File, UploadFile
 from pydantic import BaseModel
 from fastapi.responses import Response, StreamingResponse, JSONResponse
+import logging
 
 from services.GetAudio import GetAudio
+from services.SpeechAnalysis import analyze_sentiment
+from services.Video2Text import process_video_bytes
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -46,3 +52,17 @@ async def api_get_audio_dummy(request: api_get_audio_request):
 		print(f" 💥 Error in /api/get-audio-dummy e {e}")
 		return JSONResponse(content={"error":"Internal server error"}, status_code=500)
 
+
+@router.post("/api/test-speech-analysis")
+async def speech_analysis(file: UploadFile = File(...)):
+    try:
+        video_data = await file.read()
+        if(video_data):
+          logger.info("Read Video Success")
+        transcription = process_video_bytes(video_data)
+        logger.info(transcription)
+        sentiment_result = analyze_sentiment(transcription)
+        return {"transcription": transcription, "sentiment": sentiment_result.dict()}
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+      
