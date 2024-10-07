77bbcc8f1078a65b312a6bb7c8362c6b334edbe0
Arman D <adrismir@gmail.com>
2024-10-06 23:17:20 -0700

    Test runner

--- api/RunTests.py
+++ api/RunTests.py
@@ -4,12 +4,27 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 from dotenv import load_dotenv
 load_dotenv()
-import unittest
+import unittest, argparse
+
+def run_tests():
+	loader = unittest.TestLoader()
+
+	suite = loader.discover(start_dir='tests')
+
+	runner = unittest.TextTestRunner()
+	runner.run(suite)
+
 
 if __name__ == "__main__":
-    loader = unittest.TestLoader()
+	parser = argparse.ArgumentParser(description='Run tests')
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument('-a', '--all', action='store_true', help='Run all tests, including expensive ones')
+	group.add_argument('-f', '--free', action='store_true', help='Run only free tests')
+	args = parser.parse_args()
 
-    suite = loader.discover(start_dir='tests')
+	if args.all:
+		os.environ['RUN_EXPENSIVE_TESTS'] = 'true'
+	elif args.free:
+		os.environ['RUN_EXPENSIVE_TESTS'] = 'false'
 
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+	run_tests()
--- api/tests/test_GetAudio.py
+++ api/tests/test_GetAudio.py
@@ -1,9 +1,9 @@
-import unittest
-import io
+import unittest, io, os
 from services.GetAudio import GetAudio
 
 class TestGetAudio(unittest.TestCase):
 
+    @unittest.skipUnless(os.getenv('RUN_EXPENSIVE_TESTS') == 'true', "Skipping GetAudio API test to save tokens")
     def test_GetAudio(self):
         Audio = GetAudio("Hi")
         self.assertIsInstance(Audio, io.BytesIO)
