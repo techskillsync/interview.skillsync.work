a45aa3b247bb7d0e9da4283009b83cfae50f0ceb
Arman D <adrismir@gmail.com>
2024-10-07 20:34:54 -0700

    GetAudio is now a util

--- api/routes/routes.py
+++ api/routes/routes.py
@@ -1,10 +1,11 @@
 import os
-from fastapi import APIRouter, Body, File, UploadFile
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
-from fastapi.responses import Response, StreamingResponse, JSONResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 import logging
 
-from services.GetAudio import GetAudio
+from api.utils.GetAudio import GetAudio
+from services.GetQuestions import get_questions
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,37 +19,30 @@ async def home():
 class api_get_audio_request(BaseModel):
     question: str
 
-@router.post("/api/get-audio")
-async def api_get_audio(request: api_get_audio_request):
+@router.get("/api/get-questions")
+async def api_get_questions():
 	"""
-	Returns the question read aloud in the form of a BytesIO file.
+	Using the interview uuid, parses the applicant's resume, 
+	and generates questions. Returns the question text and audio
+	in the response body.
 	"""
 	try:
-		audio = GetAudio(request.question)
-
-		return StreamingResponse(audio, media_type="audio/mpeg")
+		raise Exception("Not implemented yet, use dummy endpoint")
 	except Exception as e:
-		print(f" 💥 Error in /api/get-audio - {e}")
-		return JSONResponse(content={"error":"Internal server error"}, status_code=500)
+		print(f" 💥 Error in /api/get-questions - {e}")
+		return JSONResponse(content={"error":"Internal server error in get-questions"}, status_code=500)
 
-
-@router.post("/api/get-audio-dummy")
-async def api_get_audio_dummy(request: api_get_audio_request):
+@router.get("/api/dummy-get-questions")
+async def api_dummy_get_questions(uuid: str = Query(..., description="uuid corresponding to an interview")):
 	"""
-	Returns a saved version of a question being read aloud.
+	Accepts an interview uuid, returns sample questions and
+	audio.
 	"""
-	DUMMY_AUDIO_PATH = "assets/audio_sample.mp3"
 	try:
-		import time
-		time.sleep(4)
-		if not os.path.exists(DUMMY_AUDIO_PATH):
-			return JSONResponse(content={"error": "No saved audio found"}, status_code=404)
-		audio = open(DUMMY_AUDIO_PATH, 'rb')
-		return StreamingResponse(audio, media_type="audio/mpeg")
+		get_questions(uuid)
+		raise Exception("Not implemented yet")
 	except Exception as e:
-		print(f" 💥 Error in /api/get-audio-dummy e {e}")
-		return JSONResponse(content={"error":"Internal server error"}, status_code=500)
-
-
+		print(f"  Error in /api/dummy-get-questions - {e}")
+		return JSONResponse(content={"error":"Internal server error in dummy-get-questions"}, status_code=500)
 
       
