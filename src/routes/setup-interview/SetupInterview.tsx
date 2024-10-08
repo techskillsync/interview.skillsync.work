d8260b883c643deb168ad1a2048a34d8b2c11032
Arman D <adrismir@gmail.com>
2024-10-07 19:46:13 -0700

    Questions context so entire app has questions and also can get questions on load

---
+++ src/routes/setup-interview/SetupInterview.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+function SetupInterview() {
+
+	const navigate = useNavigate()
+	const location = useLocation()
+	const [questions, setQuestions] = useState<string[]|null>(null)
+
+	async function getQuestions(): Promise<string[]> {
+		return [
+			"Introduce yourself",
+			"Tell me about the microservice you deployed at SkillSync. From what I understand microservices are meant to handle one specific task, hiding the complexity with a simple REST API. Is that correct? If so how did you ensure your microservice was simple to use while performing complicated tasks under the hood?",
+			"At UBC Agrobot what did you do when your vision for the website differed from a teammate?",
+			"I see on your resume you made a Chess Engine, tell me about an improvement you made to it. What did you change that was not working like you wanted. How did you make it work how you wanted?",
+		]
+	}
+
+	useEffect(() => {
+		if (!questions) { return }
+		navigate({ pathname: "/preinterview", search: location.search })
+	}, [questions])
+
+	useEffect(() => {
+		async function doAsync() {
+			setQuestions(await getQuestions())
+		}
+		doAsync()
+	}, [])
+
+
+	return (
+		<div className="flex flex-col justify-center items-center">
+			<h1>We are grabbing your interviewer, sit tight while she arrives.</h1>
+			<img src="/images/loading.gif" width={64}/>
+		</div>
+	)
+}
+
+export default SetupInterview
